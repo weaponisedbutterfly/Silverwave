3979d11807fe0c2d0610fbc581e55b5d0be7bb69
Solomon Osiberu <lokobos67@gmail.com>
2016-01-12 16:08:31 +0000

    Add new photo now working

--- SilverWave/app/controllers/photos_controller.rb
+++ SilverWave/app/controllers/photos_controller.rb
@@ -4,13 +4,28 @@ def feed
 end
 
 def index
+  # binding.pry
   @photos = Photo.all
 end
 
-# def show
-# end
+def show
+  @photo = Photo.find(params[:id])
+end
 
+def new
+  @photo = Photo.new
+end
 
+def edit
+  current_user.photos.edit(title: params[:photo][:title],
+    description: params[:photo][:description])
+  redirect_to(photos_path)
+end
 
+def create
+  current_user.photos.create(title: params[:photo][:title],
+    description: params[:photo][:description])
+  redirect_to(photos_path)
+end
 
 end
